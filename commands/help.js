20eb0e2449f41f5c0ae4833a1809963e32bcdc7b
Wizardpasindu07 <58454135+WizardPasindu@users.noreply.github.com>
2020-10-27 16:59:20 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports = {
         message.channel.send({
             embed:{
                 color:"GREEN",
-                description:"`1. m?kick_user - The Bot Checks All The Roles In The Server And Tells The Users Whether User Can Kick A Member.\n 2. m?ban_user - The Bot Checks All The Roles In The Server And Tells The Users Whether User Can Ban A Member.\n 3. m?kick <user name with @> <reason for kick> - Bot Will Kick The User And Give The Reason For Kicking.\n 4. m?ban <user name with @> <reason for ban> - Bot Will Ban The User And Give The Reason For Banning.\n`"
+                description:"`1. m?kick_user - The Bot Checks All The Roles In The Server And Tells The Users Whether User Can Kick A Member.\n 2. m?ban_user - The Bot Checks All The Roles In The Server And Tells The Users Whether User Can Ban A Member.\n 3. m?kick <user name with @> <reason for kick> - Bot Will Kick The User And Give The Reason For Kicking.\n 4. m?ban <user name with @> <reason for ban> - Bot Will Ban The User And Give The Reason For Banning.\n 5. m!invite - Invite This Bot To Any Server You Want.\n`"
             }
         });
     }
