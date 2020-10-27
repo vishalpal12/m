8d5aaa242294d9fe9400b53f0393a7a0da1a4ff1
Wizardpasindu07 <58454135+WizardPasindu@users.noreply.github.com>
2020-10-27 16:55:25 +0530

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,14 @@
+const { Message } = require('discord.js');
+
+module.exports = {
+   name:'inivte',
+   description:'Invite ModBot To Another Server',
+   execute(message, args){
+       message.channel.send({
+       embed:{
+           color:"GREEN",
+           description:"Invite Me To Another Server - https://discord.com/api/oauth2/authorize?client_id=769794394471006218&permissions=8&scope=bot"
+       }
+       });
+   }
+}
