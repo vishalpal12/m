27d9703fcc8a7b2a913a83f69e9b2a953f7ddf12
Wizardpasindu07 <58454135+WizardPasindu@users.noreply.github.com>
2020-10-29 22:51:01 +0530

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -7,7 +7,7 @@ module.exports = {
        message.channel.send({
        embed:{
            color:"GREEN",
-           description:"Invite Me To Another Server - https://discord.com/api/oauth2/authorize?client_id=769794394471006218&permissions=8&scope=bot"
+           description:"Invite Me To Another Server - https://discord.com/api/oauth2/authorize?client_id=769794394471006218&permissions=523382&scope=bot"
        }
        });
    }
